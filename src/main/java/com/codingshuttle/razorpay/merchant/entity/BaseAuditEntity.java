7dfa5db1840ae79b58983437bbfa0c21b684417f
chasethesky79 <seshadri.bharath@yahoo.com>
2026-07-05 00:04:06 +0530

    Created JPA entity

---
+++ src/main/java/com/codingshuttle/razorpay/merchant/entity/BaseAuditEntity.java
@@ -0,0 +1,37 @@
+package com.codingshuttle.razorpay.merchant.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseAuditEntity {
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
+
---
+++ src/main/java/com/codingshuttle/razorpay/merchant/entity/Merchant.java
@@ -0,0 +1,59 @@
+package com.codingshuttle.razorpay.merchant.entity;
+
+import com.codingshuttle.razorpay.common.enums.BusinessType;
+import com.codingshuttle.razorpay.common.enums.MerchantStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "merchant")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Merchant extends BaseAuditEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(length = 20)
+    private String contactNumber;
+
+    @Column(length = 50)
+    @Enumerated(EnumType.STRING)
+    private BusinessType businessType;
+
+    @Column(length = 50)
+    private String businessName;
+
+    @Column(length = 200)
+    private String websiteUrl;
+
+    @Column(nullable = false, length = 20)
+    private MerchantStatus status = MerchantStatus.PENDING_KYC;
+
+    @Column(length = 20)
+    private String gstId;
+
+    @Column(length = 20)
+    private String panId;
+    private String settlementBankAccount;
+
+    @Column(length = 20)
+    private String settlementBankIfsc;
+
+    @Column(length = 200)
+    private String settlementBankAccountHolderName;
+}
