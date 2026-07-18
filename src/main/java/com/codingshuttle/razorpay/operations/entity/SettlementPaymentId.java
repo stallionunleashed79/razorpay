d5e05f3285d658090b1198f8b38febb5de8c0af7
chasethesky79 <seshadri.bharath@yahoo.com>
2026-07-19 01:15:10 +0530

    Added entity for webhook event

--- src/main/java/com/codingshuttle/razorpay/operations/entity/SettlementPaymentId.java
+++ src/main/java/com/codingshuttle/razorpay/operations/entity/SettlementPaymentId.java
@@ -1,17 +1,13 @@
 package com.codingshuttle.razorpay.operations.entity;
 
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Embedded;
 
 import java.util.UUID;
 
 @Embeddable
 class SettlementPaymentId {
 
-    @Embedded
     private UUID settlementId;
 
-    @Embedded
     private UUID paymentId;
-
 }
