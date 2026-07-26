7381c94472e3365bf01f739cf9fe9fa365bc7e63
chasethesky79 <seshadri.bharath@yahoo.com>
2026-07-26 13:30:41 +0530

    Added method in auth service

--- src/main/java/com/codingshuttle/razorpay/merchant/repository/MerchantRepository.java
+++ src/main/java/com/codingshuttle/razorpay/merchant/repository/MerchantRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface MerchantRepository extends JpaRepository<Merchant, UUID> {
+    boolean existsByEmail(final String email);
 }
--- src/main/java/com/codingshuttle/razorpay/merchant/service/impl/AuthServiceImpl.java
+++ src/main/java/com/codingshuttle/razorpay/merchant/service/impl/AuthServiceImpl.java
@@ -2,6 +2,8 @@ package com.codingshuttle.razorpay.merchant.service.impl;
 
 import com.codingshuttle.razorpay.merchant.dto.request.MerchantSignupRequest;
 import com.codingshuttle.razorpay.merchant.dto.response.MerchantResponse;
+import com.codingshuttle.razorpay.merchant.repository.AppUserRepository;
+import com.codingshuttle.razorpay.merchant.repository.MerchantRepository;
 import com.codingshuttle.razorpay.merchant.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,8 +14,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class AuthServiceImpl implements AuthService {
+
+    private final MerchantRepository merchantRepository;
+    private final AppUserRepository appUserRepository;
+
     @Override
-    public @Nullable MerchantResponse signup(MerchantSignupRequest merchantSignupRequest) {
+    public @Nullable MerchantResponse signup(final MerchantSignupRequest merchantSignupRequest) {
+        if (merchantRepository.existsByEmail(merchantSignupRequest.email())) {
+            throw new RuntimeException(String.format("Email %s already exists", merchantSignupRequest.email()));
+        }
         return null;
     }
 }
