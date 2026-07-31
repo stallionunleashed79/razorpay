948ec512e38720bc18275f9577307a08f9d76059
chasethesky79 <seshadri.bharath@yahoo.com>
2026-07-31 16:22:31 +0530

    Added error response class

--- src/main/java/com/codingshuttle/razorpay/common/exception/ErrorResponse.java
+++ src/main/java/com/codingshuttle/razorpay/common/exception/ErrorResponse.java
@@ -1,8 +1,11 @@
 package com.codingshuttle.razorpay.common.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
         String errorCode,
         String errorDescription,
