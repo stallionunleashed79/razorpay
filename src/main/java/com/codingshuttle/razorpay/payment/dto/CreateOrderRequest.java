826f033f35f04e28366636612d2d360261b68f89
chasethesky79 <seshadri.bharath@yahoo.com>
2026-08-15 17:23:11 +0530

    Implemented the create order request

--- src/main/java/com/codingshuttle/razorpay/payment/dto/CreateOrderRequest.java
+++ src/main/java/com/codingshuttle/razorpay/payment/dto/CreateOrderRequest.java
@@ -1,14 +1,18 @@
 package com.codingshuttle.razorpay.payment.dto;
 
 import com.codingshuttle.razorpay.common.entity.Money;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
 public record CreateOrderRequest(
+        @Size(max = 100)
         String receipt,
         LocalDateTime expiresAt,
         Map<String, Object> notes,
+        @NotNull(message = "Amount is required")
         Money amount
 ) {
 }
