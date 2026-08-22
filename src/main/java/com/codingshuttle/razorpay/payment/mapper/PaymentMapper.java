6a8b5b551f90ed70d63fc9bc54426bc3a16c4035
chasethesky79 <seshadri.bharath@yahoo.com>
2026-08-22 14:17:17 +0530

    Added code for payment mapper

--- src/main/java/com/codingshuttle/razorpay/payment/mapper/PaymentMapper.java
+++ src/main/java/com/codingshuttle/razorpay/payment/mapper/PaymentMapper.java
@@ -3,10 +3,12 @@ package com.codingshuttle.razorpay.payment.mapper;
 import com.codingshuttle.razorpay.payment.dto.response.PaymentResponse;
 import com.codingshuttle.razorpay.payment.entity.Payment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PaymentMapper {
 
+    @Mapping(target = "orderId", source="order.id")
     PaymentResponse toPaymentResponse(Payment payment);
 }
