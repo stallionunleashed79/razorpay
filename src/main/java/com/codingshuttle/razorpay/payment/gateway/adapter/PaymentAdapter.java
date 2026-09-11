28378ec5fad30710e5a36bb2242cfe1c76975db5
chasethesky79 <seshadri.bharath@yahoo.com>
2026-09-11 11:28:05 +0530

    Implemented strategy design pattern for payment API

--- src/main/java/com/codingshuttle/razorpay/payment/gateway/adapter/PaymentAdapter.java
+++ src/main/java/com/codingshuttle/razorpay/payment/gateway/adapter/PaymentAdapter.java
@@ -3,8 +3,6 @@ package com.codingshuttle.razorpay.payment.gateway.adapter;
 import com.codingshuttle.razorpay.common.enums.PaymentMethod;
 import com.codingshuttle.razorpay.payment.gateway.dto.PaymentRequest;
 
-import javax.smartcardio.Card;
-
 public interface PaymentAdapter {
 
     void initiatePayment(PaymentRequest paymentRequest);
